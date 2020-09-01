468cbf8e4c0f34203a3d4372f019dfeeb254034c
Toadettek <oliviakitty1989@gmail.com>
2020-09-01 16:31:49 +0800

    add system message page

---
+++ pages/index/System message details.vue
@@ -0,0 +1,24 @@
+<!-- 系统通知详情界面 -->
+
+<template>
+	<view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
